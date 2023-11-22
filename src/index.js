8b48c60e9f9688119267c1f267a0012bf344e69d
muditbhatt95 <muditbhatt95@gmail.com>
2023-11-23 00:10:56 +0530

    new commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { StateProvider } from "./utils/StateProvider.jsx";
+import reducer, { initialState } from "./utils/reducer.js";
+ReactDOM.render(
+  <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
